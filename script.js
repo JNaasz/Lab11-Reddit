431327cb45c48fe0ef2a1359ed4dfbe6f142b5c4
Jennifer Naasz <naasz.jl@gmail.com>
2017-05-01 08:24:21 -0400

    without sortable

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+// this changes the welcome circle to include a text input and button
 var changeContent = function(){
   $('p').remove();
   $('h2').remove();
@@ -12,17 +13,21 @@ $('#click').click(function(){
   changeContent();
 });
 
-$('#inputButton').click(function(){
-  console.log('go');
-});
+//this makes the welcome div hidden and runs the main function to append children to the page
+$('#welcome').on('click', '#inputButton', function(){
 
-$('#inputButton').on('click', function(){
-
-   var search = $('#choice').val();
+   var search = $('#welcome input').val();
       searchReddit(search);
       console.log('clicked');
-    //  $('#welcomeHolder').css('visibility', 'hidden');
+      $('#again').css('visibility', 'visible')
+     $('.holder').css('visibility', 'hidden');
 });
+
+// this changes the visibility on the welcome circle back to visible so a new search can be run
+$('#again').on('click', function(){
+  $('.holder').css('visibility', 'visible');
+  $('#welcome input').val('');
+})
 
 //This code adds the reddit divs to the #images div---------
 function searchReddit(subreddit) {
@@ -36,28 +41,37 @@ console.log(subreddit);
       var title = i.data.title;
       var thumbnail = i.data.thumbnail;
       var author = i.data.author;
+      var link = i.data.url;
 
       if(i.data.over_18 === true){
         alert("This is not safe for work. Try again.");
       };
 
       if(i.data.post_hint === 'image' && i.data.over_18 === false){
-      $('#images').append("<div class='container'><p>" + title +
-      '</br>' + 'OP: ' + author + '</p><img src=' + thumbnail + '></div>');
+      $('#images').append("<div class='container'><p id='title'>" + title +
+      '</p>' + '<p id="op">OP: ' + author + '</p><img src=' + thumbnail + '><a href='+ link +'>link to original source</a></div>');
       };
+
+      search = "";
      });//closing for forEach()
 
    $('#images div:gt(11)').remove(); //this removes every element aftter index 11
 
-
    $('#images div').mouseover(function(){
      $(this).addClass('over');
+     $('.border').addClass('spin');
    });
 
    $('#images div').mouseout(function(){
      $(this).removeClass('over');
+     $('.border').removeClass('spin');
    });
- });// closing for $get function
+
+// $(function(){
+//    $('#container')sortable();
+// }
+
+});// closing for $get function
 
 }; //closing for on click function
 //end of appending function------------------------------------------
